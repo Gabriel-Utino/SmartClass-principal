1e1e8e179b742b72bfe8ff1177c4889b8d25b0cd
Gabriel Utino <kamishiroyuuji@gmail.com>
2024-11-05 21:07:43 -0300

    page responsavel

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,56 +4,31 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { ensureAuthenticated, authorizeRoles } = require('../middleware/authorize');
 
-// 認証関連のルート
+// 認証関連のルート AuthRoutes
 router.get('/login', (req, res) => res.render('login', { message: req.flash('error') }));
-/* router.get('/register', (req, res) => res.render('register', { message: req.flash('error') })); */
 router.post('/login', authController.login);
-
-router.post('/register', authController.register);
 router.get('/logout', authController.logout);
 
 
 // **ユーザー登録ルートを秘書と管理者に限定**
 router.get('/register',  (req, res) => res.render('register', { message: req.flash('error') }));
-router.post('/register',  authController.register);
+router.post('/register', authController.register);
 
-// paginas
+// todos
 router.get('/calendario', (req, res) => {
     res.render('calendario', { user: req.session.user }) // views/apricarFaltas.ejs
 });
-router.get('/apricarFaltas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
-    res.render('apricarFaltas', { user: req.session.user }) // views/apricarFaltas.ejs
-})
-router.get('/apricarNotas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
-    res.render('apricarNotas', { user: req.session.user }) // views/apricarNotas.ejs
-})
-router.get('/editarNotas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
-    res.render('editarNotas', { user: req.session.user }); // views/editarNotas.ejsをレンダリング
-});
 
-
-
-router.get('/variFaltas', ensureAuthenticated, authorizeRoles(3), (req, res) => {
-  res.render('variFaltas', { user: req.session.user }) // views/variFaltas.ejs
-})
-router.get('/variNotas', ensureAuthenticated, authorizeRoles(3), (req, res) => {
-  res.render('variNotas', { user: req.session.user }) // views/variNotas.ejs
-})
-
-
-
+// Admin
 router.get('/connectTurmaDisci', ensureAuthenticated, authorizeRoles(5, 1),  (req, res) => {
   res.render('connectTurmaDisci', { user: req.session.user });
 });
-
 router.get('/orgDisciTurma', ensureAuthenticated, authorizeRoles(5, 1),  (req, res) => {
   res.render('orgDisciTurma', { user: req.session.user });
 });
-
 router.get('/aluno_resp', ensureAuthenticated, authorizeRoles(5, 1),  (req, res) => {
   res.render('aluno_resp', { user: req.session.user });
 });
-
 router.get('/turma', ensureAuthenticated, authorizeRoles(5, 1), (req, res) => {
   res.render('turma', { user: req.session.user }) // views/turma.ejs
 })
@@ -73,14 +48,37 @@ router.get('/professor', ensureAuthenticated, authorizeRoles(5, 1), (req, res) =
   res.render('professor', { user: req.session.user }) // views/professor.ejs
 })
 
+// Professores
+router.get('/apricarFaltas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
+    res.render('apricarFaltas', { user: req.session.user }) // views/apricarFaltas.ejs
+})
+router.get('/apricarNotas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
+    res.render('apricarNotas', { user: req.session.user }) // views/apricarNotas.ejs
+})
+router.get('/editarNotas', ensureAuthenticated, authorizeRoles(2), (req, res) => {
+    res.render('editarNotas', { user: req.session.user }); // views/editarNotas.ejs
+});
+
+// Alunos
+router.get('/variNotas', ensureAuthenticated, authorizeRoles(3), (req, res) => {
+  res.render('variNotas', { user: req.session.user }) // views/variNotas.ejs
+})
+router.get('/variFaltas', ensureAuthenticated, authorizeRoles(3), (req, res) => {
+  res.render('variFaltas', { user: req.session.user }) // views/variFaltas.ejs
+})
+
+// Responsaveis
+router.get('/veriResponsavel', ensureAuthenticated, authorizeRoles(4), (req, res) => {
+  res.render('veriResponsavel', { user: req.session.user }) // views/veriResponsavel.ejs
+})
+
+
+
 
 router.get('/template', (req, res) => {
-  res.render('template') // views/professor.ejs
+  res.render('template') // views/template.ejs
 })
 
-router.get('/mural', (req, res) => {
-  res.render('mural') // views/professor.ejs
-})
 
 // パスワードリセット関連のルート
 router.get('/forgot-password', authController.getForgotPassword);
