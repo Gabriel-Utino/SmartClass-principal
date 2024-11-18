205053310730df53cf86129fc72a0874c263e484
amorimthaisss <112404581+amorimthaisss@users.noreply.github.com>
2024-11-18 11:06:00 -0300

    update

--- routes/calendarioRoutes.js
+++ routes/calendarioRoutes.js
@@ -4,15 +4,15 @@ const calendarioController = require('../controllers/calendarioController');
 const { ensureAuthenticated, authorizeRoles } = require('../middleware/authorize');
 
 // Rota para listar todos os eventos (permite acesso ao administrador e professor)
-router.get('/listar', ensureAuthenticated, authorizeRoles(1, 2), calendarioController.getAllEventos);
+router.get('/listar', ensureAuthenticated, authorizeRoles(1, 2, 5), calendarioController.getAllEventos);
 
 // Rota para criar um novo evento (permite acesso ao administrador e professor)
-router.post('/criar', ensureAuthenticated, authorizeRoles(1, 2), calendarioController.createEvento);
+router.post('/criar', ensureAuthenticated, authorizeRoles(1, 2, 5), calendarioController.createEvento);
 
 // Rota para editar um evento existente (permite acesso ao administrador e professor)
-router.put('/editar/:id_evento', ensureAuthenticated, authorizeRoles(1, 2), calendarioController.updateEvento);
+router.put('/editar/:id_evento', ensureAuthenticated, authorizeRoles(1, 2, 5), calendarioController.updateEvento);
 
 // Rota para excluir um evento (permite acesso ao administrador e professor)
-router.delete('/deletar/:id_evento', ensureAuthenticated, authorizeRoles(1, 2), calendarioController.deleteEvento);
+router.delete('/deletar/:id_evento', ensureAuthenticated, authorizeRoles(1, 2, 5), calendarioController.deleteEvento);
 
 module.exports = router;
