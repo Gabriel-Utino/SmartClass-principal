d0c5fdcf6627270f99c4cdd4626cfd16c0888ece
Gabriel Utino <kamishiroyuuji@gmail.com>
2024-12-03 07:30:56 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -189,6 +189,12 @@ app.get('/organizacao', (req, res) => {
   }
   res.render('organizacao', { user: req.session.user }) // ダッシュボードを表示
 })
+app.get('/orgDisciTurma', (req, res) => {
+  if (!req.session.user) {
+    return res.redirect('/login') // ユーザーがログインしていない場合
+  }
+  res.render('orgDisciTurma', { user: req.session.user }) // ダッシュボードを表示
+})
 app.get('/alunosTurma', (req, res) => {
   if (!req.session.user) {
     return res.redirect('/login') // ユーザーがログインしていない場合
